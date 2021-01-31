e492dc87f20c77ec4084067993171286b64c9c43
jnsbrdbr <jinu.b@liv.com>
2021-01-31 21:53:57 +0330

    edit

--- final.m
+++ final.m
@@ -6,28 +6,37 @@ close all;
 % 'office_4.jpg'
 % 'greens.jpg'
 % 'hands1.jpg'
-% 'C:\Users\jinu\Dropbox\My PC (DESKTOP-4UNBIL6)\Desktop\6.tiff'
+% 'C:\Users\jinu\Dropbox\My PC (DESKTOP-4UNBIL6)\Desktop\2.tiff'
+% 'cameraman.tif'
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-mypic = imread('office_4.jpg');
+mypic = imread('C:\Users\jinu\Dropbox\My PC (DESKTOP-4UNBIL6)\Desktop\2.tiff');
 subplot(4,2,2);
-image(mypic);
+imshow(mypic)
 title 'low contras picture';
-axis off;        
-axis image;
+if(size(mypic,3)==3)
+    
+    YCbCr = rgb2ycbcr(mypic);
+    subplot(4,2,1);
+    imshow(YCbCr);
+    title 'converting rgb to YCbCr';
+
+    subplot(4,2,3)
+    Y = YCbCr(:,:,1);
+    imshow(Y);
+    title ('picture of channel Y','fontsize',8);
+    
+else
+    
+    subplot(4,2,3)
+    Y=mypic;
+    imshow(Y);
+    title ('picture of channel Y','fontsize',8);
+    
+end
 
 
-YCbCr = rgb2ycbcr(mypic);
-subplot(4,2,1);
-imshow(YCbCr);
-title 'converting rgb to YCbCr';
-
-
-subplot(4,2,3)
-Y = YCbCr(:,:,1);
-imshow(Y);
-title ('picture of channel Y','fontsize',8);
 
 
 
@@ -37,7 +46,7 @@ title ('histogram of channel Y','fontsize',8);
 
 [pixelCounts grayLevels] = imhist(Y);
 
-stdy=std(pixelCounts); %calculate standard deviation of each channel.
+stdy=std(pixelCounts); %calculate standard deviation of channel.
 
 ABMy = stdy + double(pixelCounts); %as the article said:additional based histogram modification:hi+delta which delta is standard deviation
 
@@ -89,37 +98,63 @@ end
 U=idct2(B);
 
 
-wq=cat(3,U,YCbCr(:,:,2),YCbCr(:,:,3));
-
-subplot(4,2,7) 
- imshow(wq);
-title ('image of concatenating of 3 channle','fontsize',8);
-
-myfinalpic = ycbcr2rgb(wq);
-subplot(4,2,8) 
- imshow(myfinalpic);
-title ('final image','fontsize',8);
+if(size(mypic,3)==3)
+    
+    wq=cat(3,U,YCbCr(:,:,2),YCbCr(:,:,3));
+    subplot(4,2,7) 
+    imshow(wq);
+    title ('image of concatenating of 3 channle','fontsize',8);
+    
+    myfinalpic = ycbcr2rgb(wq);
+    subplot(4,2,8) 
+    imshow(myfinalpic);
+    title ('final image','fontsize',8);
+else
+    U=uint8(U);
+    myfinalpic=U;
+    subplot(4,2,8) 
+    imshow(myfinalpic);
+    title ('final image','fontsize',8);
+    
+    
+end
 
 figure
-HSV = rgb2hsv(mypic);
-Heq = histeq(HSV(:,:,3));
-HSV_mod = HSV;
-HSV_mod(:,:,3) = Heq;
-RGB = hsv2rgb(HSV_mod);
+if(size(mypic,3)==3)
+    
+    
+    HSV = rgb2hsv(mypic);
+    Heq = histeq(HSV(:,:,3));
+    HSV_mod = HSV;
+    HSV_mod(:,:,3) = Heq;
+    RGB = hsv2rgb(HSV_mod);
 
-subplot(2,2,1)
-imshow(mypic);
-title ('original image','fontsize',8);
+    subplot(2,2,1)
+    imshow(mypic);
+    title ('original image','fontsize',8);
 
-subplot(2,2,2)
-imshow(RGB);
-title ('HE','fontsize',8);
+    subplot(2,2,2)
+    imshow(RGB);
+    title ('HE','fontsize',8);
 
-subplot(2,2,3)
-imshow(myfinalpic);
-title ('myfinalpic','fontsize',8);
+    subplot(2,2,3)
+    imshow(myfinalpic);
+    title ('myfinalpic','fontsize',8);
+    
+else
+    RGB=histeq(mypic);
+    subplot(2,2,1)
+    imshow(mypic);
+    title ('original image','fontsize',8);
 
+    subplot(2,2,2)
+    imshow(RGB);
+    title ('HE','fontsize',8);
 
+    subplot(2,2,4)
+    imshow(myfinalpic);
+    title ('myfinalpic','fontsize',8);
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%psnr
@@ -142,6 +177,7 @@ ssimval2 = ssim(mypic,uint8(RGB));
 %%%%%%%%%%%%%%%ambe
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if(size(mypic,3)==3)
 B = rgb2gray(mypic);
 B2 = rgb2gray(myfinalpic);
 B3 = rgb2gray(uint8(RGB));
@@ -150,7 +186,13 @@ Z2 = mean2(B2);
 Z3 = mean2(B3);
 AMBE1 = Z1-Z2; % calculate AMBE1
 AMBE2 = Z1-Z3; % calculate AMBE2
-
+else
+    Z1 = mean2(mypic); 
+    Z2 = mean2(myfinalpic); 
+    Z3 = mean2(uint8(RGB));
+    AMBE1 = Z1-Z2; % calculate AMBE1
+    AMBE2 = Z1-Z3; % calculate AMBE2
+end
 
 %%%%%%%%%%%
 %%%end
